91c5144725bf290c3df736b594f6ce053f4e1afd
jeritter <jeritter@protonmail.com>
2022-06-21 19:57:38 -0600

    New branch and updating directories

---
+++ build.sh
@@ -0,0 +1,4 @@
+cd tech-blog
+cat templates/header.html content/index.html templates/footer.html > docs/index.html
+cat templates/header.html content/about.html templates/footer.html > docs/about.html
+cat templates/top.html content/blog.html templates/footer.html > docs/blog.html
