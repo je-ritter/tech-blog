ac6040f2149af4bfd0e615d23742ed8b218e40a1
jeritter <jeritter@protonmail.com>
2022-06-21 20:04:40 -0600

    Status Site BASH now added and running.

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
-cd tech-blog
 cat templates/header.html content/index.html templates/footer.html > docs/index.html
 cat templates/header.html content/about.html templates/footer.html > docs/about.html
-cat templates/top.html content/blog.html templates/footer.html > docs/blog.html
+cat templates/header.html content/blog.html templates/footer.html > docs/blog.html
