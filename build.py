bd2a7c3e8828329b7a86e9d5ff3377ee810c29fe
jeritter <jeritter@protonmail.com>
2022-06-29 17:55:36 -0600

    Updated indentation

--- build.py
+++ build.py
@@ -6,15 +6,15 @@ def main():
 
     content = open('./content/index.html').read()
     index_html = header_template + content + footer_template
-    open('./docs/python/index.html', 'w+').write(index_html)
+    open('./docs/index.html', 'w+').write(index_html)
 
     content = open('./content/blog.html').read()
     blog_html = header_template + content + footer_template
-    open('./docs/python/blog.html', 'w+').write(blog_html)
+    open('./docs/blog.html', 'w+').write(blog_html)
 
     content = open('./content/about.html').read()
     about_html = header_template + content + footer_template
-    open('./docs/python/about.html', 'w+').write(about_html)
+    open('./docs/about.html', 'w+').write(about_html)
 
 
 # pages = [
@@ -39,8 +39,20 @@ def main():
 #print(page_title)
 
 #for page in pages:
-#    print(page_title)
+#    print(page)
 
 
 main()
 
+
+#Code from Michael to try: 
+#for page in pages:
+#    page_title = page['title']
+#    main ()
+#    print(page_title)
+
+#Another Hint from Micahel:
+
+#for ....
+#    input_filename = page['filename']
+#    content = open(input_filename).read() # removing hardcoded string
