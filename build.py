f194cb202845a5ee6852ba5cac26c062c412b519
jeritter <jeritter@protonmail.com>
2022-06-29 11:46:24 -0600

    updated to include a cleaner tree

--- build.py
+++ build.py
@@ -4,14 +4,15 @@ header_template = open('./templates/header.html').read()
 footer_template = open('./templates/footer.html').read()
 
 content = open('./content/index.html').read()
-
 index_html = header_template + content + footer_template
-open('index.html', 'w+').write(index_html)
+open('./docs/python/index.html', 'w+').write(index_html)
 
 content = open('./content/blog.html').read()
 blog_html = header_template + content + footer_template
-open('blog.html', 'w+').write(blog_html)
+open('./docs/python/blog.html', 'w+').write(blog_html)
 
 content = open('./content/about.html').read()
 about_html = header_template + content + footer_template
-open('about.html', 'w+').write(about_html)
+open('./docs/python/about.html', 'w+').write(about_html)
+
+
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-cat templates/header.html content/index.html templates/footer.html > docs/index.html
-cat templates/header.html content/about.html templates/footer.html > docs/about.html
-cat templates/header.html content/blog.html templates/footer.html > docs/blog.html
+cat templates/header.html content/index.html templates/footer.html > /home/excitum/tech-blog/docs/bash/index.html
+cat templates/header.html content/about.html templates/footer.html > /home/excitum/tech-blog/docs/bash/about.html
+cat templates/header.html content/blog.html templates/footer.html > /home/excitum/tech-blog/docs/bash/blog.html
